1a844f47e79e7820f32f3e31a1a5e9c82ca4a540
hyperingenious <skbmasale941@gmail.com>
2025-04-19 04:18:34 +0530

    FIX in code org

--- src/appwrite/crud/read.js
+++ src/appwrite/crud/read.js
@@ -1,30 +1,22 @@
-import { databaseID, databases, productCollectionId } from "../appwrite";
+import {
+  databaseID,
+  databases,
+  productCollectionId,
+  productRequestsCollectionId,
+} from "../appwrite";
 async function readProductById(id) {
-        try {
-            const document= await databases.getDocument(
-                databaseID,
-                productCollectionId,
-                id
-            );
-            return document;
-        } catch (error) {
-            console.error(error)
-            throw error;
-        }
-    }
+  try {
+    const document = await databases.getDocument(
+      databaseID,
+      productCollectionId,
+      id
+    );
+    return document;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
-    async function listAllProducts() {
-        try {
-            const documents= await databases.listDocuments(
-                databaseID,
-                productCollectionId,
-                id
-            );
-            return documents;
-        } catch (error) {
-            console.error(error)
-            throw error;
-        }
-    }
 
     export {readProductById, listAllProducts}
