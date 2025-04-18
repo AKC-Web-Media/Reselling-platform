cf6c1d150c0e6bbda53a50bf8fadfae31a6c5cb1
hyperingenious <skbmasale941@gmail.com>
2025-04-19 04:02:55 +0530

    UPDATE: docs updated for the fuctions

--- src/appwrite/crud/create.js
+++ src/appwrite/crud/create.js
@@ -1,7 +1,21 @@
 import { ID } from "appwrite";
-import { databaseID, databases, productCollectionId } from "../appwrite";
+import { databaseID, databases, productCollectionId, productRequestsCollectionId } from "../appwrite";
 
-// image2, image3, image4 are optional
+/**
+ * Creates a new product document in the database
+ * @param {Object} params - The product parameters
+ * @param {string} params.userId - ID of the user creating the product
+ * @param {string} params.category - Category of the product
+ * @param {string} params.product_name - Name of the product
+ * @param {string} params.product_description - Description of the product
+ * @param {number} params.price - Price of the product
+ * @param {string} params.image1 - Primary image URL of the product
+ * @param {string} [params.image2] - Optional second image URL
+ * @param {string} [params.image3] - Optional third image URL
+ * @param {string} [params.image4] - Optional fourth image URL
+ * @returns {Promise<Object>} The created document object
+ * @throws {Error} If document creation fails
+ */
 async function createProduct({
     userId,
     category,
@@ -35,7 +49,20 @@ async function createProduct({
         throw error;
     }
 }
-export {createProduct}async function createPurchaseRequest({product, to , bid_price, name, message, contact_no}) {
+
+/**
+ * Creates a new purchase request document in the database
+ * @param {Object} params - The purchase request parameters
+ * @param {string} params.product - ID of the product being requested
+ * @param {string} params.to - ID of the seller receiving the request
+ * @param {number} params.bid_price - Offered price for the product
+ * @param {string} params.name - Name of the buyer
+ * @param {string} params.message - Message to the seller
+ * @param {string} params.contact_no - Contact number of the buyer
+ * @returns {Promise<Object>} The created document object
+ * @throws {Error} If document creation fails
+ */
+async function createPurchaseRequest({product, to , bid_price, name, message, contact_no}) {
     try {
         const document = await databases.createDocument(
             databaseID,
@@ -50,3 +77,5 @@ export {createProduct}async function createPurchaseRequest({product, to , bid_pr
         throw error;
     }
 }
+
+export {createProduct, createPurchaseRequest}
