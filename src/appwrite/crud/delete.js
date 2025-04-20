cc6d8523327d5d415c37b14c6ed86b3521ad45ae
hyperingenious <skbmasale941@gmail.com>
2025-04-20 12:02:30 +0530

    ADD: method add to delete product

--- src/appwrite/crud/delete.js
+++ src/appwrite/crud/delete.js
@@ -14,3 +14,16 @@ async function deletePurchaseRequestById(id) {
     throw e;
   }
 }
+
+async function deleteProductById(id) {
+  try {
+    await databases.deleteDocument(databaseID, productCollectionId, id);
+    console.log(`Purchase request deleted successfully!`);
+
+    return null
+  } catch (e) {
+    throw e;
+  }
+}
+
+export {deleteProductById, deletePurchaseRequestById}
